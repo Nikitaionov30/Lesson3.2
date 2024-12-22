5915e4ee40f71a9b03d32dccacddd298c6c64315
Nikitaionov30 <nikitaionov30091998@gmail.com>
2024-12-22 19:20:47 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,17 @@
+def send_email(message, recipient,*, sender = "university.help@gmail.com"):
+    variants = ('.com', '.ru', '.net')
+    if recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif not recipient.endswith(variants) or not sender.endswith(variants) or '@' not in recipient or '@' not in sender:
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес ' + recipient)
+    elif sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса ' + sender + ' на адрес ' + recipient)
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ' + sender + ' на адрес ' + recipient)
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
